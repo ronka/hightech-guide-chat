62da292bf12b15ceed062b1a350c591d722f193f
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-07-08 13:57:02 +0300

    Remove old envs

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,9 +3,7 @@ import z from "zod";
 const envSchema = z.object({
   OPENAI_API_KEY: z.string().trim().min(1),
   PINECONE_API_KEY: z.string().trim().min(1),
-  // PINECONE_ENVIRONMENT: z.string().trim().min(0),
   PINECONE_INDEX_NAME: z.string().trim().min(1),
-  // PDF_PATH: z.string().trim().min(1),
   INDEX_INIT_TIMEOUT: z.coerce.number().min(1),
   ARCJET_KEY: z.string().trim().min(1),
 });
