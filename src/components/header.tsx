2a8c0b62e5967c2c32e6933d9583185fff0e5f47
Ron Kantor <ronkantor@google.com>
2024-08-23 17:30:11 +0300

    add beta badge

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import { Badge } from "./ui/badge";
+
 const Header = () => {
   return (
     <header className="flex justify-center bg-background px-4 py-3 shadow-sm sm:px-6">
@@ -5,7 +7,10 @@ const Header = () => {
         <div className="rounded-full w-8 h-8 bg-[#55efc4] flex items-center justify-center text-2xl">
           🤖
         </div>
-        <div className="font-medium">הייטקיסטGPT</div>
+        <div className="flex gap-2">
+          <span className="font-medium">הייטקיסטGPT</span>
+          <Badge variant={"outline"}>beta</Badge>
+        </div>
       </div>
     </header>
   );
