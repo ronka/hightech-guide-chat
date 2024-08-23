1b57b51b7954c86d5f9616f1db71a37c304fa88d
Ron Kantor <ronkantor@google.com>
2024-08-23 18:00:22 +0300

    add purchase links to chat context

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -2,3 +2,10 @@ export const BOOK_PROMO = {
   EVRIT: "https://hightechguide.co.il/evrit-book-chat-promo",
   STEIMATZKY: "https://hightechguide.co.il/steimatzky-book-chat-promo",
 };
+
+export const CHAT_LINKS = {
+  PURCHASE: {
+    EVRIT: "https://hightechguide.co.il/evrit-book-chat-message",
+    STEIMATZKY: "https://hightechguide.co.il/steimatzky-book-chat-promo",
+  },
+};
--- src/services/prompt-templates.ts
+++ src/services/prompt-templates.ts
@@ -1,3 +1,5 @@
+import { CHAT_LINKS } from "@/constants/links";
+
 // Creates a standalone question from the chat-history and the current question
 export const STANDALONE_QUESTION_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
@@ -13,7 +15,14 @@ https://hightechguide.co.il/iftach-bar-salary-talk
 https://hightechguide.co.il/cs-facebook-group
 
 אם מבקשים לינק לתוכנית לימוד שלי באוניברסיטה הפתוחה:
-https://hightechguide.co.il/learning-porgram`;
+https://hightechguide.co.il/learning-porgram
+
+לינק לקניית עותק פיזי ודיגיטלי דרך סטימצקי:
+${CHAT_LINKS.PURCHASE.STEIMATZKY}
+
+לינק לקניית עותק דיגיטלי דרך סטימצקי:
+${CHAT_LINKS.PURCHASE.EVRIT}
+`;
 
 // Actual question you ask the chat and send the response to client
 export const QA_TEMPLATE = `You are an enthusiastic AI assistant. Use the following pieces of context to answer the question at the end.
@@ -21,7 +30,9 @@ If you don't know the answer, just say you don't know. DO NOT try to make up an 
 If the question is not related to the context, try to answer anyway with the context you find but you MOST mention that doesn't show precisely in the book.
 your answers should be in Hebrew.
 
+<links>
 ${USEFUL_LINKS}
+</links>
 
 {context}
 
