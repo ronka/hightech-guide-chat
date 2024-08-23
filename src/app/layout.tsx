398b9ed9b7e135aa81b282c46d9f190c263ebcf9
Ron Kantor <ronkantor@google.com>
2024-08-23 17:46:32 +0300

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,25 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" dir="rtl" suppressHydrationWarning>
-        <head />
+        <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </head>
         <body>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
