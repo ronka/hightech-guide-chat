ed1b2c5434bd3240b8a8bdce0f5dc58fa2ad43e7
Ron Kantor <ronkamail@gmail.com>
2024-08-28 07:28:25 +0300

    fix redirects

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,14 +1,23 @@
 import { redirect } from "next/navigation";
 
 export default function Page({ params }: { params: { slug: string } }) {
-  if (params.slug === "iftach-bar-salary-talk") {
-    redirect("https://www.youtube.com/watch?v=pzq37L4UBUU");
-  } else if (params.slug === "cs-facebook-group") {
-    redirect("https://www.facebook.com/groups/154179721990088");
-  } else if (params.slug === "learning-porgram") {
-    redirect(
-      "https://docs.google.com/spreadsheets/d/1K0vX803HCqLOVxkcRnNqsMS2QEc_VHyRlD3y5QRuI3Q/edit?usp=sharing"
-    );
+  switch (params.slug) {
+    case "iftach-bar-salary-talk":
+      redirect("https://www.youtube.com/watch?v=pzq37L4UBUU");
+    case "cs-facebook-group":
+      redirect("https://www.facebook.com/groups/154179721990088");
+    case "learning-porgram":
+      redirect(
+        "https://docs.google.com/spreadsheets/d/1K0vX803HCqLOVxkcRnNqsMS2QEc_VHyRlD3y5QRuI3Q/edit?usp=sharing"
+      );
+    case "evrit-book-chat-promo":
+    case "evrit-book-chat-message":
+      redirect(
+        "https://www.e-vrit.co.il/Product/33185/%D7%94%D7%9E%D7%93%D7%A8%D7%99%D7%9A_%D7%9C%D7%94%D7%99%D7%99%D7%98%D7%A7%D7%99%D7%A1%D7%98_%D7%94%D7%9E%D7%AA%D7%97%D7%99%D7%9C"
+      );
+    case "steimatzky-book-chat-promo":
+    case "steimatzky-book-chat-message":
+      redirect("https://www.steimatzky.co.il/011360920");
   }
 
   redirect("/");
--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,16 +1,16 @@
 export const BOOK_PROMO = {
-  EVRIT: "https://hightechguide.co.il/evrit-book-chat-promo",
-  STEIMATZKY: "https://hightechguide.co.il/steimatzky-book-chat-promo",
+  EVRIT: "/evrit-book-chat-promo",
+  STEIMATZKY: "/steimatzky-book-chat-promo",
 };
 
 export const CHAT_LINKS = {
   PURCHASE: {
-    EVRIT: "https://hightechguide.co.il/evrit-book-chat-message",
-    STEIMATZKY: "https://hightechguide.co.il/steimatzky-book-chat-promo",
+    EVRIT: "/evrit-book-chat-message",
+    STEIMATZKY: "/steimatzky-book-chat-promo",
   },
   FROM_THE_BOOK: {
-    CS_FACEBOOK_GROUP: "https://hightechguide.co.il/cs-facebook-group",
-    LEARNING_PROGRAM: "https://hightechguide.co.il/learning-porgram",
-    IFTAH_BAR_SALARY_TALK: "https://hightechguide.co.il/iftach-bar-salary-talk",
+    CS_FACEBOOK_GROUP: "/cs-facebook-group",
+    LEARNING_PROGRAM: "/learning-porgram",
+    IFTAH_BAR_SALARY_TALK: "/iftach-bar-salary-talk",
   },
 };
