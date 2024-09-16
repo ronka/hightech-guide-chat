b3904cf20a3f3aa9f1fdc2c35b4b960485c73c17
Ron Kantor <ronkamail@gmail.com>
2024-09-16 21:39:09 +0300

    unify header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import { Footer } from "@/components/footer";
-import { Header } from "@/components/landing-page/header";
+import { Header } from "@/components/header";
 import { Main } from "@/components/landing-page/main";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { BookOpen, ShoppingCart, User, Quote } from "lucide-react";
-import Link from "next/link";
 
 export default function LandingPage() {
   return (
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,65 @@
+"use client";
+
+import { cn } from "@/services/utils";
+import { BookOpen, Linkedin } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Badge } from "./ui/badge";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const isChatPage = pathname === "/chat";
+
   return (
-    <header className="flex justify-center bg-background px-4 py-3 shadow-sm sm:px-6">
-      <div className="flex items-center gap-4">
+    <header className="px-4 lg:px-6 h-14 flex items-center gap-20 justify-center">
+      <Link className="flex gap-4 items-center" href="/">
         <div className="rounded-full w-8 h-8 bg-[#55efc4] flex items-center justify-center text-2xl">
           🤖
+          <span className="sr-only">המדריך להייטיקיסט המתחיל</span>
         </div>
-        <div className="flex gap-2">
-          <span className="font-medium">הייטקיסטGPT</span>
+
+        <div className={cn("flex gap-2", !isChatPage && "opacity-0")}>
+          <span className="text-sm font-medium hover:underline underline-offset-4">
+            הייטקיסטGPT
+          </span>
           <Badge variant={"outline"}>beta</Badge>
         </div>
-      </div>
+      </Link>
+
+      <nav className="flex gap-4 sm:gap-6">
+        <Link
+          className={cn(
+            "text-sm font-medium hover:underline underline-offset-4",
+            isChatPage && "underline"
+          )}
+          href="/chat"
+        >
+          לצ׳אט
+        </Link>
+        <Link
+          className={cn(
+            "text-sm font-medium hover:underline underline-offset-4",
+            pathname === "/" && "underline"
+          )}
+          href="/"
+        >
+          לספר
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="/#contact"
+        >
+          צור קשר
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          target="_blank"
+          href="https://www.ronka.dev"
+        >
+          לבלוג
+        </Link>
+      </nav>
     </header>
   );
 };
