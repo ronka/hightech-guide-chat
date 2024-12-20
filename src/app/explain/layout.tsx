adace8b18c2edf8eaf47d3b6e87d891fc2788dc3
Ron Kantor <ronkamail@gmail.com>
2024-12-20 16:42:18 +0200

    cleanup

--- src/app/explain/layout.tsx
+++ src/app/explain/layout.tsx
@@ -1,11 +1,7 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
-import { Main } from "@/components/landing-page/main";
-
 export default function ExplainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="flex flex-col min-h-screen light">{children}</div>;
+  return <div className="flex flex-col min-h-screen">{children}</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Main } from "@/components/landing-page/main";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen light">
+    <div className="flex flex-col min-h-screen">
       <Main />
     </div>
   );
