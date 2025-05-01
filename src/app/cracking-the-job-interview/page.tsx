9eba3f4c64f2fd22a58ebb4a2ed463226dbaf13c
Ron Kantor <ronkamail@gmail.com>
2025-05-01 21:04:04 +0300

    improve copy

--- src/app/cracking-the-job-interview/page.tsx
+++ src/app/cracking-the-job-interview/page.tsx
@@ -483,10 +483,7 @@ export default function Home() {
       </section>
 
       {/* Testimonials Section */}
-      {/* <section
-        id="testimonials"
-        className="w-full py-12 md:py-24 lg:py-32 bg-muted"
-      >
+      <section id="testimonials" className="w-full py-12 md:py-24 lg:py-32">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
@@ -649,7 +646,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </section> */}
+      </section>
     </main>
   );
 }
