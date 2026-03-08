83d87a060d7253b75beaf984837adf7ad99a2350
ronka <ronkamail@gmail.com>
2026-03-08 16:25:22 +0200

    update unncessary env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,13 +1,11 @@
 import z from "zod";
 
 const envSchema = z.object({
-  OPENAI_API_KEY: z.string().trim().min(1),
   PINECONE_API_KEY: z.string().trim().min(1),
   PINECONE_INDEX_NAME: z.string().trim().min(1),
   INDEX_INIT_TIMEOUT: z.coerce.number().min(1),
   UPSTASH_REDIS_REST_URL: z.string().trim().min(1),
   UPSTASH_REDIS_REST_TOKEN: z.string().trim().min(1),
-  GOOGLE_GENERATIVE_AI_API_KEY: z.string().trim().min(1),
 });
 
 export const env = envSchema.parse(process.env);
